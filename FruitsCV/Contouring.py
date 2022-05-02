5bca47008f74ff94cfcc3560483d97acd71e8a09
PunkUnited <leonid.chesnikov@gmail.com>
2022-05-02 11:26:05 +0300

    My research in computer vision

--- FruitsCV/Contouring.py
+++ FruitsCV/Contouring.py
@@ -7,7 +7,7 @@ for dock in glob("ImagesTest/*"):
     files = glob(dock + '/*.jpg')
     for myFile in files:
         image = cv2.imread(myFile)
-        blur = cv2.medianBlur(image, 7)
+        blur = cv2.medianBlur(image, 5)
         gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
         _, binary = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
         element = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
